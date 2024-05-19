33772c6bc7015decea1a714b65a10abf83e1b007
hatarino <rikuhatano09@gmail.com>
2024-05-19 19:15:25 +0900

    add neovim clipboard config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,4 +2,7 @@
 vim.o.termguicolors = true
 
 -- Key mappings.
+-- You may need to install clipboard provider like xclip or wl-clipboard.
 vim.keymap.set('n', 'gy', '"+y')
+vim.keymap.set('v', 'gy', '"+y')
+vim.keymap.set('x', 'gy', '"+y')
